560919ea9c34a98fda2c1f57c0d2ca73f9ae27fa
youzunzhi <youzunzhi@163.com>
2020-06-28 18:01:53 +0800

    no training log

--- eval.py
+++ eval.py
@@ -1,58 +1,7 @@
-import torch
-import argparse
-from yacs.config import CfgNode as CN
-import numpy as np
-from data import get_data_iter
-from model import TextCNN
-from utils import handle_keyboard_interruption, handle_other_exception, setup_logger, log_info, get_load_path
-torch.manual_seed(0)
-cfg = CN()
-cfg.CUDA = torch.cuda.is_available()
-cfg.DATASET_NAME = 'movie'
-cfg.FILTER_NUM = 100
-cfg.FILTER_SIZE = [3, 4, 5]
-cfg.EMBEDDING_DIM = 128
-cfg.PRETRAINED_EMBEDDING = True
-cfg.PRETRAINED_PATH = 'pretrained/sgns.zhihu.word'
-cfg.FINETUNE_EMBEDDING = False
-cfg.MULTICHANNEL = False    # use 2 channels of word embedding
-cfg.DROPOUT_RATE = 0.5
-cfg.EXPERIMENT_NAME = f'baseline'
-# ---------
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("opts", help="Modify configs using the command-line", default=None, nargs=argparse.REMAINDER)
-args = parser.parse_args()
-cfg.merge_from_list(args.opts)
-# ---------
-if cfg.EXPERIMENT_NAME == 'baseline':
-    cfg.PRETRAINED_EMBEDDING = False
-    cfg.FINETUNE_EMBEDDING = False
-    cfg.MULTICHANNEL = False
-elif cfg.EXPERIMENT_NAME == 'pretrain':
-    cfg.PRETRAINED_EMBEDDING = True
-    cfg.FINETUNE_EMBEDDING = False
-    cfg.MULTICHANNEL = False
-elif cfg.EXPERIMENT_NAME == 'pretrain_finetune':
-    cfg.PRETRAINED_EMBEDDING = True
-    cfg.FINETUNE_EMBEDDING = True
-    cfg.MULTICHANNEL = False
-elif cfg.EXPERIMENT_NAME == 'multichannel':
-    cfg.PRETRAINED_EMBEDDING = True
-    cfg.FINETUNE_EMBEDDING = True
-    cfg.MULTICHANNEL = True
-else:
-    raise NotImplementedError
-cfg.BATCH_SIZE = 64     # useless
-cfg.SHOW_MISTAKES = False
-cfg.EXPERIMENT_NAME += f'_{cfg.DATASET_NAME}'
-# cfg.LOAD_PATH = get_load_path(cfg.EXPERIMENT_NAME)
-print(cfg)
-
-
 def main():
     _, _, test_dataiter, embedding_vectors = get_data_iter(cfg)
-    # model = TextCNN(cfg, embedding_vectors, cfg.LOAD_PATH)
-    model = TextCNN(cfg, embedding_vectors)
+    model = TextCNN(cfg, embedding_vectors, cfg.LOAD_PATH)
+    # model = TextCNN(cfg, embedding_vectors)
     if cfg.CUDA:
         model.cuda()
     evaluate('test', model, test_dataiter, cfg.CUDA, cfg.SHOW_MISTAKES)
@@ -92,4 +41,54 @@ def evaluate(split, model, eval_dataiter, use_cuda, show_mistakes=False):
 
 
 if __name__ == '__main__':
+    import torch
+    import argparse
+    from yacs.config import CfgNode as CN
+    import numpy as np
+    from data import get_data_iter
+    from model import TextCNN
+    from utils import log_info, get_load_path
+
+    torch.manual_seed(0)
+    cfg = CN()
+    cfg.CUDA = torch.cuda.is_available()
+    cfg.DATASET_NAME = 'movie'
+    cfg.FILTER_NUM = 100
+    cfg.FILTER_SIZE = [3, 4, 5]
+    cfg.EMBEDDING_DIM = 128
+    cfg.PRETRAINED_EMBEDDING = True
+    cfg.PRETRAINED_PATH = 'pretrained/sgns.zhihu.word'
+    cfg.FINETUNE_EMBEDDING = False
+    cfg.MULTICHANNEL = False  # use 2 channels of word embedding
+    cfg.DROPOUT_RATE = 0.5
+    cfg.EXPERIMENT_NAME = f'baseline'
+    # ---------
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("opts", help="Modify configs using the command-line", default=None, nargs=argparse.REMAINDER)
+    args = parser.parse_args()
+    cfg.merge_from_list(args.opts)
+    # ---------
+    if cfg.EXPERIMENT_NAME == 'baseline':
+        cfg.PRETRAINED_EMBEDDING = False
+        cfg.FINETUNE_EMBEDDING = False
+        cfg.MULTICHANNEL = False
+    elif cfg.EXPERIMENT_NAME == 'pretrain':
+        cfg.PRETRAINED_EMBEDDING = True
+        cfg.FINETUNE_EMBEDDING = False
+        cfg.MULTICHANNEL = False
+    elif cfg.EXPERIMENT_NAME == 'pretrain_finetune':
+        cfg.PRETRAINED_EMBEDDING = True
+        cfg.FINETUNE_EMBEDDING = True
+        cfg.MULTICHANNEL = False
+    elif cfg.EXPERIMENT_NAME == 'multichannel':
+        cfg.PRETRAINED_EMBEDDING = True
+        cfg.FINETUNE_EMBEDDING = True
+        cfg.MULTICHANNEL = True
+    else:
+        raise NotImplementedError
+    cfg.BATCH_SIZE = 64  # useless
+    cfg.SHOW_MISTAKES = False
+    cfg.EXPERIMENT_NAME += f'_{cfg.DATASET_NAME}'
+    cfg.LOAD_PATH = get_load_path(cfg.EXPERIMENT_NAME)
+    print(cfg)
     main()
